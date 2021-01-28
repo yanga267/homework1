6c17b8b315776f68250c264e653f7351dc3918f4
yanga267 <77342482+yanga267@users.noreply.github.com>
2021-01-28 02:23:34 -0500

    Create homework1.py

---
+++ homework1.py
@@ -0,0 +1,24 @@
+#question1
+q1 = ['red', 'blue']
+
+
+#question2
+q2 = []
+for i in range(11):
+    q2.append(30+3*i)
+    
+#question3
+q3 = {"Sunny": "play", "Rainy": "watching TV", "Cloudy": "walk"}
+
+#question4
+def q4(score):
+    if score < 70:
+        grade = "F"
+    elif score < 80:
+        grade = "C"
+    elif score < 90:
+        grade = "B"
+    else:
+        grade = "A"
+        
+    return grade
